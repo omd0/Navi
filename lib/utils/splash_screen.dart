8e154bcdeed1d42a1d446bdce2125f7a57947426
unknown <Emad Fussi>
2024-06-17 02:16:34 +0300

    last computer

--- lib/utils/splash_screen.dart
+++ lib/utils/splash_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     // Your splash screen UI code goes here
-    return Scaffold(
+    return const Scaffold(
       body: Center(
         child: Text('Splash Screen'),
       ),
